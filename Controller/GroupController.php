f16106024e23101a40fc7ff93bfeb80a93b31df2
fagottino <anto.orla@gmail.com>
2017-01-13 22:03:57 +0100

    sample

--- Controller/GroupController.php
+++ Controller/GroupController.php
@@ -88,7 +88,13 @@ class GroupController {
         try {
             $db = Database::getConnection();
             //$sql = "SELECT ".DB_PREFIX."user.id_telegram, ".DB_PREFIX."user.name FROM ".DB_PREFIX."user JOIN ".DB_PREFIX."user_group ON ".DB_PREFIX."user.id_telegram = ".DB_PREFIX."user_group.id_user JOIN ".DB_PREFIX."group ON ".DB_PREFIX."user_group.id_group = ".DB_PREFIX."group.id_group WHERE ".DB_PREFIX."user_group.id_group = '".$_chat->getId()."' AND ".DB_PREFIX."user_group.active = '1' AND ".DB_PREFIX."user_group.id_user != '".$_me->result->id."'";
-            $sql = "SELECT ".DB_PREFIX."user.id_telegram, ".DB_PREFIX."user.name FROM ".DB_PREFIX."user JOIN ".DB_PREFIX."user_group ON ".DB_PREFIX."user.id_telegram = ".DB_PREFIX."user_group.id_user WHERE ".DB_PREFIX."user_group.id_group = '".$_chat->getId()."' AND ".DB_PREFIX."user_group.active = '1' AND ".DB_PREFIX."user_group.id_user != '".$_me->result->id."'";
+            $sql = "SELECT ".DB_PREFIX."user.id_telegram, ".DB_PREFIX."user.name"
+                    . "FROM ".DB_PREFIX."user "
+                    . "JOIN ".DB_PREFIX."user_group ON ".DB_PREFIX."user.id_telegram = ".DB_PREFIX."user_group.id_user "
+                    . "WHERE ".DB_PREFIX."user_group.id_group = '".$_chat->getId()."'"
+                    . "AND ".DB_PREFIX."user_group.active = '1'"
+                    . "AND ".DB_PREFIX."user_group.id_user != '".$_me->result->id."'"
+                    . "AND ".DB_PREFIX."user_group.active = '1'";
             $query = $db->query($sql);
             
             if ($query->num_rows > 0) {
@@ -113,21 +119,20 @@ class GroupController {
             
             $sql = "SELECT ".DB_PREFIX."user.id_telegram, ".DB_PREFIX."user.name FROM ".DB_PREFIX."user JOIN ".DB_PREFIX."user_group ON ".DB_PREFIX."user.id_telegram = ".DB_PREFIX."user_group.id_user JOIN ".DB_PREFIX."paid_coffee ON ".DB_PREFIX."user_group.id_group = ".DB_PREFIX."paid_coffee.id_group WHERE ".DB_PREFIX."user_group.id_group = '".$_user->getChat()->getId()."' AND ".DB_PREFIX."user_group.active = '1' AND ".DB_PREFIX."user_group.id_user != '".$_me->result->id."' AND ".DB_PREFIX."user.id_telegram NOT IN (SELECT ".DB_PREFIX."paid_coffee_people.id_user FROM ".DB_PREFIX."paid_coffee JOIN ".DB_PREFIX."paid_coffee_people ON ".DB_PREFIX."paid_coffee.id_paid_coffee = ".DB_PREFIX."paid_coffee_people.id_paid_coffee WHERE ".DB_PREFIX."paid_coffee.id_group != '".$_user->getChat()->getId()."' AND ".DB_PREFIX."paid_coffee.set_by = '".$_user->getIdTelegram()."' AND ".DB_PREFIX."paid_coffee.powered_by IS NULL)";
             
-// // funziona
-//SELECT coffee_user.id_telegram, coffee_user.name FROM coffee_user
-//	JOIN coffee_user_group ON coffee_user.id_telegram = coffee_user_group.id_user
-//	JOIN coffee_paid_coffee ON coffee_user_group.id_group = coffee_paid_coffee.id_group
-//	WHERE coffee_user_group.id_group = '-114342037'
-//	AND coffee_user_group.active = '1'
-//	AND coffee_user_group.id_user != '186132931'
-//	AND coffee_user.id_telegram NOT IN
-//	(SELECT coffee_paid_coffee_people.id_user FROM coffee_paid_coffee
-//	JOIN coffee_paid_coffee_people ON coffee_paid_coffee.id_paid_coffee = coffee_paid_coffee_people.id_paid_coffee
-//	WHERE coffee_paid_coffee.id_group != '186132931'
-//	AND coffee_paid_coffee.set_by = '19179842'
-//	AND coffee_paid_coffee.powered_by IS NULL
-//	)
-            
+            // // funziona
+            $sql = "SELECT ".DB_PREFIX."user.id_telegram, ".DB_PREFIX."user.name FROM ".DB_PREFIX."user "
+                    . "JOIN ".DB_PREFIX."user_group ON ".DB_PREFIX."user.id_telegram = ".DB_PREFIX."user_group.id_user "
+                    . "JOIN ".DB_PREFIX."paid_coffee ON ".DB_PREFIX."user_group.id_group = ".DB_PREFIX."paid_coffee.id_group "
+                    . "WHERE ".DB_PREFIX."user_group.id_group = '".$_user->getChat()->getId()."' "
+                    . "AND ".DB_PREFIX."user_group.id_user != '".$_me->result->id."' "
+                    . "AND ".DB_PREFIX."user_group.active = '1' "
+                    . "AND ".DB_PREFIX."user.id_telegram NOT IN "
+                    . "(SELECT ".DB_PREFIX."paid_coffee_people.id_user FROM ".DB_PREFIX."paid_coffee "
+                    . "JOIN ".DB_PREFIX."paid_coffee_people ON ".DB_PREFIX."paid_coffee.id_paid_coffee = ".DB_PREFIX."paid_coffee_people.id_paid_coffee "
+                    . "WHERE ".DB_PREFIX."paid_coffee.id_group = '".$_user->getChat()->getId()."' "
+                    . "AND ".DB_PREFIX."paid_coffee.set_by = '".$_user->getIdTelegram()."' "
+                    . "AND ".DB_PREFIX."paid_coffee.powered_by IS NULL)";
+
             $query = $db->query($sql);
             
             if ($query->num_rows > 0) {
@@ -137,7 +142,7 @@ class GroupController {
                 $db->close();
                 return $res;
             } else {
-                throw new GroupControllerException($lang->error->noResultsFound);
+                throw new GroupControllerException($lang->error->noResultsFound."AFASVAFF");
             }
         } catch (DatabaseException $ex) {
             throw new DatabaseException($ex->getMessage().$lang->general->line.$ex->getLine().$lang->general->code.$ex->getCode());
